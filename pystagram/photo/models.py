7187508d0ae8848be33067ac9e5197c23eb137a8
Kay Cha <iam@hannal.net>
2014-10-05 03:02:08 +0900

    4편 admin.py과 models.py

--- pystagram/photo/models.py
+++ pystagram/photo/models.py
@@ -3,7 +3,12 @@
 from django.db import models
 
 class Photo(models.Model):
-    image_file = models.ImageField()
-    filtered_image_file = models.ImageField()
-    description = models.TextField(max_length=500)
+    image_file = models.ImageField(upload_to='static_files/uploaded/%Y/%m/%d')
+    filtered_image_file = models.ImageField(upload_to='static_files/uploaded/%Y/%m/%d')
+    description = models.TextField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def delete(self, *args, **kwargs):
+        self.image_file.delete()
+        self.filtered_image_file.delete()
+        super(Photo, self).delete(*args, **kwargs)
