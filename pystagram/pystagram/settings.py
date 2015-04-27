4a5bceed9b5d6922aa1562fad593731f3c08e3f0
Hannal <iam@hannal.net>
2015-04-27 11:59:51 +0900

    STATIC_URL 실수 수정

--- pystagram/pystagram/settings.py
+++ pystagram/pystagram/settings.py
@@ -81,7 +81,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/assets/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_statics')
 
