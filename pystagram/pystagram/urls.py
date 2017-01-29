b320ede98f689a6f7e3bf1a989534e29d562c22f
Kay Cha <kay@hannal.net>
2017-01-30 00:45:18 +0900

    Add login/logout urls

--- pystagram/pystagram/urls.py
+++ pystagram/pystagram/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 from photos.views import hello
 from photos.views import detail
@@ -13,6 +14,19 @@ urlpatterns = [
     url(r'^photos/(?P<pk>[0-9]+)/$', detail, name='detail'),
     url(r'^photos/upload/$', create, name='create'),
     url(r'^admin/', admin.site.urls),
+    url(
+        r'^accounts/login/',
+        auth_views.login,
+        name='login',
+        kwargs={
+            'template_name': 'login.html'
+        }
+    ),
+    url(
+        r'^accounts/logout/',
+        auth_views.logout,
+        name='logout'
+    ),
 ]
 
 urlpatterns += static('/upload_files/', document_root=settings.MEDIA_ROOT)
