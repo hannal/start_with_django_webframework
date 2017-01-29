1f802232e5019bb52a76053c2bcb07eeaeb08f65
Kay Cha <kay@hannal.net>
2017-01-29 23:39:29 +0900

    Add staticfiles settings

--- pystagram/pystagram/settings.py
+++ pystagram/pystagram/settings.py
@@ -118,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/upload_files/'
 
