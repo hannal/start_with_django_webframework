aadf74a009363546f184a9f1d41ff0772e32f719
Kay Cha <kay@hannal.net>
2017-01-30 01:38:54 +0900

    Add show user's photographs

--- pystagram/profiles/views.py
+++ pystagram/profiles/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 
 
 def profile(request, username):
-    ctx = {}
+    User = get_user_model()
+    user = get_object_or_404(User, username=username)
+    ctx = {
+        'user': user,
+    }
     return render(request, 'profile.html', ctx)
 
